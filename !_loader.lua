66e0567f8005ce9df3b33c5f52e99f33eb36c571
hersheyzchoco-cmyk <hersheyzchoco@gmail.com>
2026-08-02 14:23:51 +0530

    Update !_loader.lua

--- !_loader.lua
+++ !_loader.lua
@@ -11,7 +11,7 @@ local KEY_FILE = "IBdihPHub_SavedKey.txt"
 
 -- ═══ BANNED USERS (add usernames or user IDs here) ═══
 local BANNED_NIGGAS = {
-    "8kruo",
+    "Lost nigga",
     "PlaceholderUsername2",
     "PlaceholderUsername3",
 }
